f89010739e71fa991b86cc3f146f5a617c2d1d13
Evan Travers <evantravers@gmail.com>
2022-03-20 00:45:21 -0500

    Fix UUID and file import

--- things-to-taskwarrior.js
+++ things-to-taskwarrior.js
@@ -44,8 +44,9 @@
 
   const stringToUuid = (str) => {
     str = str.replace('-', '');
-    return 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c, p) {
-      return str[p % str.length];
+    return 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
     });
   }
 
@@ -82,5 +83,5 @@
     )
   })
 
-  writeTextToFile(JSON.stringify(tasks), "tasks.json")
+  writeTextToFile(tasks.map(t => JSON.stringify(t)).join("\n"), "tasks.json")
 })();
