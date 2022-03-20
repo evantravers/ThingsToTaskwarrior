4d0ade3e9fe2e663eea0c8ab3d69719915ffe7de
Evan Travers <evantravers@gmail.com>
2022-03-20 10:51:49 -0500

    Fix the timestamp
    Solves #3

--- things-to-taskwarrior.js
+++ things-to-taskwarrior.js
@@ -39,7 +39,8 @@
   }
 
   const ISOdate = function(date) {
-    return date.toISOString().replace(/[^A-Z0-9]/g, '')
+    // YYYYMMDDTHHMMSSZ
+    return `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, '0')}${date.getDate().toString().padStart(2, '0')}T${date.getHours().toString().padStart(2, '0')}${date.getMinutes().toString().padStart(2, '0')}${date.getSeconds().toString().padStart(2, '0')}Z`
   }
 
   const stringToUuid = (str) => {
