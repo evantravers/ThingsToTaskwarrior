1a3c397c8330b5fcb7bba2fdddd3507748eb920d
Evan Travers <evantravers@gmail.com>
2022-03-24 08:47:49 -0500

    Fix UTC offset

--- things-to-taskwarrior.js
+++ things-to-taskwarrior.js
@@ -35,7 +35,7 @@
 
   const ISOdate = function(date) {
     // YYYYMMDDTHHMMSSZ
-    return `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, '0')}${date.getDate().toString().padStart(2, '0')}T${date.getHours().toString().padStart(2, '0')}${date.getMinutes().toString().padStart(2, '0')}${date.getSeconds().toString().padStart(2, '0')}Z`
+    return `${date.getUTCFullYear()}${(date.getUTCMonth() + 1).toString().padStart(2, '0')}${date.getUTCDate().toString().padStart(2, '0')}T${date.getUTCHours().toString().padStart(2, '0')}${date.getUTCMinutes().toString().padStart(2, '0')}${date.getUTCSeconds().toString().padStart(2, '0')}Z`
   }
 
   const stringToUuid = (str) => {
